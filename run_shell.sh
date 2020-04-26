96cb92530e066e5d47edbaf7d0b99d251d18c143
Nagaraju <vpp.nagaraju1234@gmail.com>
2020-04-26 17:27:58 +0530

    shell for input and output dir

---
+++ run_shell.sh
@@ -0,0 +1,16 @@
+input=$1
+list_file=$2
+output=$3
+
+
+#echo $input
+
+mkdir output
+
+for file in $input/*
+do
+  f=$(basename "$file")
+  echo "Running $f"
+  python3 t1_t2_t3_match_and_replace.py $file $list_file > "$3/$f".out
+  #python3 t1_t2_t3_match_and_replace-xls.py $file $list_file > "$3/$f".out
+done
