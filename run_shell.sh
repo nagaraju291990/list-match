57c09aa4e612f3f9da5291421a5954cb14a1e1c4
Nagaraju <vpp.nagaraju1234@gmail.com>
2020-04-26 18:02:45 +0530

    updated shell for mkdir  -p

--- run_shell.sh
+++ run_shell.sh
@@ -5,12 +5,12 @@ output=$3
 
 #echo $input
 
-mkdir output
+mkdir $output -p
 
 for file in $input/*
 do
   f=$(basename "$file")
   echo "Running $f"
-  python3 t1_t2_t3_match_and_replace.py $file $list_file > "$3/$f".out
-  #python3 t1_t2_t3_match_and_replace-xls.py $file $list_file > "$3/$f".out
+  #python3 t1_t2_t3_match_and_replace.py $file $list_file > "$3/$f".out
+  python3 t1_t2_t3_match_and_replace-xls.py $file $list_file > "$3/$f".out
 done
